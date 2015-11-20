b5b7bcee0910677fcf6ae663926600e778d582f6
= <kmtao88@gmail.com>
2015-11-20 15:30:26 -0500

    Added ListView, now can display data from density

---
+++ app/src/main/java/com/adicu/density/LocationListAdapter.java
@@ -0,0 +1,64 @@
+package com.adicu.density;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Adapter that converts data to location list view.
+ */
+public class LocationListAdapter extends BaseAdapter {
+    private Context mContext;
+    private ArrayList<DensityData> mDensityData;
+
+    public LocationListAdapter(Context context, ArrayList<DensityData> locations) {
+        mContext = context;
+        mDensityData = locations;
+    }
+
+    public int getCount() {
+        return mDensityData.size();
+    }
+
+    public Object getItem(int position) {
+        return mDensityData.get(position);
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
+        RelativeLayout itemView;
+        if (convertView == null) {
+            itemView = (RelativeLayout) inflater.inflate(R.layout.location_item, parent, false);
+        }
+        else {
+            itemView = (RelativeLayout) convertView;
+        }
+
+        TextView locationNameView = (TextView) itemView.findViewById(R.id.locationName);
+        String name = mDensityData.get(position).getGroupName();
+        locationNameView.setText(name);
+        return itemView;
+    }
+
+    /**
+     * Updates list view data and refreshes the view.
+     * @param densityData the new location names.
+     */
+    public void update(ArrayList<DensityData> densityData) {
+        mDensityData = densityData;
+        notifyDataSetChanged();
+    }
+
+}
